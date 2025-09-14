8d8c776cb77425b505d79ca7ff3c8b5c8948d0db
BathlaNeffi <123646861+BathlaNeffi@users.noreply.github.com>
2025-09-14 10:09:15 +0530

    used remote DB 2.02 version

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,16 @@
 // getting-started.js
 const mongoose = require('mongoose');
 
-main().catch(err => console.log(err));
+main().catch(err => console.log("Error in connecting***************",err));
 main().then(()=> console.log("Connected to DB"));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/todo_app_db2');
+//   await mongoose.connect('mongodb://127.0.0.1:27017/todo_app_db2');
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+//   use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+
+await mongoose.connect(`mongodb://${process.env.TODO_APP_REMOTE_dB}`);
 }
+
+
+
